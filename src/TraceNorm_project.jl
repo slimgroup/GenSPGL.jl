58232ff4d4fb875c67cc8f4d92ac474a60fd6ea9
Mike Anderson <michaela@dugeo.com>
2018-10-15 09:00:03 +0800

    Fix invalid if in TraceNorm_project.jl

--- src/TraceNorm_project.jl
+++ src/TraceNorm_project.jl
@@ -5,7 +5,10 @@ Force the rows of L and R to have norm at most B
 """
 function TraceNorm_project{ETx<:Number, Tx<:AbstractVector{ETx}}(x::Tx, B, weights, params::Dict{String,Any})
 
-if isapprox(norm(x), 0.0) && warn("norm(x) cannot be 0 in TraceNorm_project")
+if isapprox(norm(x), 0.0)
+    warn("norm(x) cannot be 0 in TraceNorm_project")
+end
+  
 c = sqrt(B/(0.5*norm(x).^2))
 xout = min(1,c).*x
 
